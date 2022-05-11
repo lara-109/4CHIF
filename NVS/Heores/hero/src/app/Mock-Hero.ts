bff5547f0ff3ac194f404394717e029e7aac09c9
winkler.lara <winkler.lara@student.htlwrn.ac.at>
2022-05-11 18:24:44 +0200

    re

---
+++ NVS/Heores/hero/src/app/Mock-Hero.ts
@@ -0,0 +1,15 @@
+import {Hero} from "./hero";
+
+
+export const Heroes : Hero[] = [
+  { id: 11, name: 'Dr Nice', power: 23},
+  { id: 12, name: 'Narco', power: 24},
+  { id: 13, name: 'Bombasto', power: 67},
+  { id: 14, name: 'Celeritas', power: 39},
+  { id: 15, name: 'Magneta', power: 32},
+  { id: 16, name: 'RubberMan', power: 100},
+  { id: 17, name: 'Dynama', power: 21},
+  { id: 18, name: 'Dr IQ', power: 23},
+  { id: 19, name: 'Magma', power: 39},
+  { id: 20, name: 'Tornado', power: 92}
+]
